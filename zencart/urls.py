414c51d7ffd8e302647a267bc9e62806afbfb76f
Mushraf Muhammed <mushrafmuhammed@outlook.com>
2023-05-21 08:06:05 +0530

    seller-app initial commit

--- zencart/urls.py
+++ zencart/urls.py
@@ -19,5 +19,11 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+<<<<<<< Updated upstream
+=======
+    path('common/', include('common.urls')),
+    path('seller/', include('seller.urls')),
+
+>>>>>>> Stashed changes
 
 ]
